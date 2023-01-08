c73ff9ef46441b14aa98c13f9a09d5704b61f375
zizwiz <zizwiz@engineer.com>
2023-01-08 17:58:25 +0000

    Initial upload of empty Solution.
    Empty solution compiles and runs but that is all.

---
+++ Create a MS Word Report/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Create_a_MS_Word_Report
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text += " : v" + Assembly.GetExecutingAssembly().GetName().Version; // put in the version number
+        }
+    }
+}
